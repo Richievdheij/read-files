9b127214c17836b49b653496fbb0735f9ad41ed1
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2022-05-09 10:08:18 +0200

    TheZenOfPython
    Zoek op internet naar “The Zen of Python” en kopieer deze tekst naar de README.md van je repository
    
    Maak nu een programmatje die elke seconden een regel uit de README.md in de terminal laat zien.

--- TheZenOfPython.py
+++ TheZenOfPython.py
@@ -4,6 +4,6 @@ line = True
 file = open("README.md", "r")   # Open the file to read ("r"
 
 while line:
-    line = file.read()          # Read the file and save it in a variable
+    line = file.readline()          # Read the file and save it in a variable
     print(line)                 # Print to the console
     sleep(1)                    # Print to the console every second
